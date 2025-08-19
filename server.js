88a00134658c3db6366a2314aed955b148052b36
Y-ash-Thakur <yasht0509@gmail.com>
2025-08-19 20:53:35 +0530

    accessing the absolute and relative path to the resource

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 import http from 'node:http';
+import path from 'node:path';
 
 const PORT = 8000;
 
-// const __dirname = (import.meta.dirname) // dirname is globally available in this file now 
-
-console.log(import.meta.dirname);
-
-console.log(process.cwd()); // This will give you the current working directory
-// it's just the folder from which we launch our node project
+const __dirname = (import.meta.dirname);
 
 const server = http.createServer((req, res) => {
+
+  const absPathToResource = path.join(__dirname, 'public', 'index.html')
+  const relPathToResource = path.join('public', 'index.html')
+  console.log('absolute: ', absPathToResource)
+  console.log('relative: ', relPathToResource)
+
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html')
+    res.setHeader('Content-Type', 'text/html') 
     // res.writeHead(200, {'Content_type': 'text/html'});
     res.end('<html><h1>The server is working </h1></html>')
 })
