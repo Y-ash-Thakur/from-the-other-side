2097716d935456289744588114f594ea33d74780
Y-ash-Thakur <yasht0509@gmail.com>
2025-08-21 17:13:06 +0530

    Implemented the routeHandler for GET request

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import http from 'node:http';
 import { serveStatic } from './utils/serveStatic.js';
 import { getData } from './utils/getData.js';
+import { handleGet } from './handlers/routeHandlers.js'
 
 
 const PORT = 8000;
@@ -11,7 +12,13 @@ console.log( await getData());
 
 const server = http.createServer( async(req, res) => {
 
-    await serveStatic(req, res, __dirname); 
+    if(req.url.startsWith('/api') && req.method === 'GET'){
+        return await handleGet(res);
+    }
+
+    else if (!req.url.startsWith('/api')) {
+        return await serveStatic(req, res, __dirname) 
+    }
 
 })
 
